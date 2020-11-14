ffdcc667c24a7e4dc15e7caf73f966f641e22037
Apurba Adhikari <erikduke182@gmail.com>
2020-11-14 11:33:32 +0545

    Test

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-import random
-print("Hello World")
+
+def main(name):
+    print("Hello " + name)
+
+main("Apurba")
+
+
+class Encryptor:
+
+    def __init__(self, message, key):
+        self.message = message
+        self.key = key
+
+    def word(self):
+        pass
+
+    def file(self):
+        pass
+
+
+usr = Encryptor("Hi, Ashutosh", "12345")
+
+print(type(usr))
