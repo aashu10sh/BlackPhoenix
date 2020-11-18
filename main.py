c72b14d19b032defbf78435c8ee44088a3322f47
apurbadh <apurba4790@gmail.com>
2020-11-18 19:14:04 +0545

    Comments Added

--- main.py
+++ main.py
@@ -1,69 +1,67 @@
+#Importing platform module for finding out the os they are using
 import platform
-print(platform.system())
+import random #Importing random for getting random numbers
 
-
+#Creating a Object called Encryptor which encrypts 
 class Encryptor:
 
-  def __init__(self, password):
-    self.password = password
-    
 
-  def file(self, filename):
-    passw = self.passw
-    f = open(filename)
-    for lines in f:
-        a = password + lines
-
-
-  def word (self, word):
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    enc = ""
-    passw = self.password
-    
-    for w in word:
-      aword = ord(w) + 0
-      aword = chr(aword)
-      enc += aword
-    print(enc)
-      
-class Decryptor:
-
+    #As all the instance needs password taking it while initializing it
     def __init__(self, password):
         self.password = password
     
+    #Creating a instance which encrypts a file
+    def file(self, filename):
+        passw = self.password #Acessing password from init
+        f = open(filename) #Opening the file which name is given by the user
+        for lines in f:
+            a = password + lines
+
+    #Creating another instance which encrypts words
+    def word (self, word):
+        enc = "" #Creating a variable for final word
+        passw = self.password #Acessing password from init
+        #Looping for every letter in the word
+        for w in word:
+            aword = ord(w) + 0 #Changing it into its ascii code and adding a rando number
+            aword = chr(aword) #Changing back to normal character
+            enc += aword #Adding all the letters to final enc
+        print(enc)
+
+#Creating another object for decrypting
+class Decryptor:
+    #Initializing and taking the password value
+    def __init__(self, password):
+        self.password = password
+    
+    #Creating a instance for the word
     def word (self, enc):
-        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        passw = self.password
+        passw = self.password #Getting the password from init
+        #Looping for every word given by the user
         for k in enc:
-            dword = ord(k) - 0
-            dword = chr(dword)
+            dword = ord(k) - 0 #Converting into ASCII and subtracting what was added while encrypinng
+            dword = chr(dword) #Converting back into character
         print(dword)
 
     
-
+#Creating a constructor function which constructs the object from user input
 def construct(ctype, tail, passw, passvalue):
-    cond = True
-    if ctype == "encrypt":
+    cond = True #Creating a variable called cond and setting it to true
+    if ctype == "encrypt": #Encrypting if the user types encrypt
         enc = Encryptor(passw)
-    elif ctype == 'decrypt':
+    elif ctype == 'decrypt': #Decryping if the user types decrypt
         enc = Decryptor(passw)
-    else:
+    else: # If the arguement is wrong setting cond to false
         cond = False
-    if (cond):
+    if (cond): #Setting condition for files and words
         if tail == '-w':
             enc.word(passvalue)
         elif tail == '-f':
             enc.file(passvalue)
-    else:
-        print("Invalid Command")
+    else: 
+        print("Invalid Command")#Priniting Invalid if the command does not exist
 
         
-
-
-
-
-a = Encryptor("test")
-a.word("Samrat")
  
 #The Message below is a help message for the program
 helpc = '''
@@ -78,19 +76,19 @@ helpc = '''
 ######################################
 '''
 while True:
-    usr_input = input("[Black Pheonix]# ")
-    firsts = usr_input[:7]
-    seconds = usr_input[7:9]
-    if usr_input[:4] == "help":
+    usr_input = input("[Black Pheonix]# ") #Printing it to look like a console
+    firsts = usr_input[:7] #Taking first seven character from user input
+    seconds = usr_input[7:9] #Taking 8-9 letter from user input
+    if usr_input[:4] == "help": #If users type help printing the help string
         print(helpc)
-    elif usr_input[:4] == "exit":
+    elif usr_input[:4] == "exit":#If user types exit, breaking the loop
         break
-    elif usr_input[:7] in ["encrypt", "decrypt"]:
-        passw = input("Enter the password : ")
-        ctype = usr_input[:7]
-        tail = usr_input[8:10]
-        latt = usr_input[11:]
-        construct("encrypt",tail, passw, latt)
+    elif usr_input[:7] in ["encrypt", "decrypt"]: #Checking if the user input is in the list or not
+        passw = random.choice([1,3,4])#Making a password a random number
+        ctype = usr_input[:7]#Filtering the type of command into ctype
+        tail = usr_input[8:10] #Taking the tail of command(file or word)
+        latt = usr_input[11:] #Taking the file name or word
+        construct(ctype,tail, passw, latt) #Passing it all to construct function
 
 
 
